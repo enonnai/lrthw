2bae0510b264f7c5abc5c537f2bebe2f0c34c156
Cristina Iannone <iannonecristina@gmail.com>
2017-03-20 13:20:03 +0000

    Add ex03

---
+++ ex03/drill_01.rb
@@ -0,0 +1,25 @@
+#prints to the console some text between quotes
+puts "I will now count my chickens:"
+#prints to the console some text between quotes, which includes the result of some maths operations
+puts "Hens #{25 + 30 / 6}"
+#prints to the console some text between quotes, which includes the result of some maths operations
+puts "Roosters #{100 - 25 * 3 % 4}"
+#prints to the console some text between quotes
+puts "Now I will count the eggs:"
+#prints the result of some maths operations
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#prints to the console some text between quotes
+puts "Is it true that 3 + 2 < 5 - 7?"
+#prints the result of some maths operations
+puts 3 + 2 < 5 - 7
+#prints to the console some text between quotes, which includes the result of some maths operations
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+#prints to the console some text between quotes
+puts "Oh, that's why it's false."
+#prints to the console some text between quotes
+puts "How about some more."
+#prints to the console some text between quotes, which includes the result of some maths operations
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater? or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
