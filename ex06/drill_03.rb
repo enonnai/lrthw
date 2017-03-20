70b4f39f4874b6976f26f3cbc7203ee3906c2e0b
Cristina Iannone <iannonecristina@gmail.com>
2017-03-20 16:37:04 +0000

    Add ex07

---
+++ ex06/drill_03.rb
@@ -0,0 +1,22 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}." #string inside of a string (x2)
+
+
+puts x
+puts y
+
+puts "I said: #{x}." #string inside of a string
+puts "I also said: '#{y}'." #string inside of a string
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
---
+++ ex07/drill_01.rb
@@ -0,0 +1,26 @@
+#prints a string
+puts "Mary had a little lamb."
+#prints a string.The singe quotes tell Ruby not to evaluate any variables inside of them
+puts "Its fleece was white as #{'snow'}."
+#prints a string
+puts "And everywhere that Mary went."
+#prints "." ten times
+puts "." * 10
+
+# below we are assigning strings of una character to 12 variables called end1 up to end12.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# 'puts' adds a newline to the end of the output, while 'print' does not. Hence the output of line 25 and 26 will be 'Cheeseburger' on one line.
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
