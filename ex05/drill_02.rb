2831c58c4a5246c1c2a676824d71f4c5bc2e3e0f
Cristina Iannone <iannonecristina@gmail.com>
2017-03-20 15:14:38 +0000

    Add ex05

---
+++ ex05/drill_02.rb
@@ -0,0 +1,11 @@
+inches = 62.99
+pounds = 114.64
+
+inch = 1
+cms_in_an_inch = 2.54
+
+pound = 1
+kgs_in_a_pound = 0.45359237
+
+puts centimetres = (cms_in_an_inch * inches).round
+puts kilograms = (pounds * kgs_in_a_pound).round
