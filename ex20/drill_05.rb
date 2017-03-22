c2161b855978acda010837cf8ce761008e03228f
Cristina Iannone <iannonecristina@gmail.com>
2017-03-22 22:49:06 +0000

    Add adjustments

--- ex20/drill_05.rb
+++ ex20/drill_05.rb
@@ -12,14 +12,14 @@ def print_a_line(line_count, f)
   puts "#{line_count}, #{f.gets.chomp}"
 end
 
-current_file = open(input_file)
+current_file = File.open(input_file)
 
 puts "First let's print the whole file:\n"
 print_all(current_file)
 
 puts "Now let's rewind, kind of like a tape."
 
-rewind(current_file)
+#rewind(current_file)
 
 puts "Let's print three lines:"
 
@@ -31,3 +31,5 @@ print_a_line(current_line, current_file)
 
 current_line += 1
 print_a_line(current_line, current_file)
+
+f.close
